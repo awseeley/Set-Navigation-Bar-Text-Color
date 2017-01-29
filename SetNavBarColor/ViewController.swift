c47d0188a6ddf816b372cdedefe0725221a9acb7
Andrew Seeley <awseeley@gmail.com>
2017-01-29 18:38:29 +1100

    Updated for Swift 3
    Updated for Swift 3

--- SetNavBarColor/ViewController.swift
+++ SetNavBarColor/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationController?.navigationBar.barTintColor = UIColor.redColor()
+        self.navigationController?.navigationBar.barTintColor = UIColor.red
         //self.navigationController?.navigationBar.barTintColor = UIColor(red: 100/255, green: 100/255, blue: 100/255, alpha: 1)
         
     }
